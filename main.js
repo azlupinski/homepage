c1e5e96c57ea18a3e557fc0024393ef00b2a126f
alupinski <azlupinski@gmail.com>
2020-06-03 10:58:34 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const name = 'Adam';
+const age = 27;
+console.log(`Siema nazywam się ${name} i mam ${age} lat. Mam nadzieję że chociaż content jest słaby to meme jest na poziomie.`);
+console.log("Takie przywitanie chyba starczy");
